fc8608d9ca7d2e6eea849c5beb9391e20db13822
AldoLaraCanto <aealc_97@hotmail.com>
2020-06-30 22:27:25 -0500

    Sistema

---
+++ sistema/includes/header.php
@@ -0,0 +1,21 @@
+<?php 
+
+	if(empty($_SESSION['active']))
+	{
+		header('location: ../login.php');
+	} 
+?>
+<header>
+		<div class="header">
+			<h1> Sistema Web Inventario UV</h1>
+			<div class="optionsBar">
+				<p>Boca del Rio, Veracruz, <?php echo fecha(); ?> </p>
+				<span>|</span>
+				<span class="user">Usuario: <?php echo $_SESSION['nombre']; ?></span>
+				<img class="photouser" src="img/user.png" alt="Usuario">
+				
+				<a href="salir.php"><i class="fas fa-sign-out-alt fa-2x" color="white" alt="Salir del sistema" title="Salir"></i></a>
+			</div>
+		</div>
+		<?php include "nav.php"; ?>
+</header>
