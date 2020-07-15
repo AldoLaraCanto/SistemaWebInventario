0506537fdb92f23f9de88b9d13a4163fc6c3e127
AldoLaraCanto <aealc_97@hotmail.com>
2020-07-14 23:14:29 -0500

    sistema web inventario

---
+++ 403.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="error404/style2.css">
+    
+</head>
+<body>
+    <img style="width: 150px; height: 150px;" src="img/superintendente2.jpg">
+    <h1>Error 403</h1>
+    <h3>¡Acceso prohibido!</h3>
+    <div>
+        <a href="sistema/index.php" class='btn'>Regresar a la Página de Inicio</a>
+    </div>   
+</body>
+</html>
